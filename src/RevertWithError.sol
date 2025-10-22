5f495344d9e79e8f783508520bc7c96f2fe4139a
garwin-cc <aabond8192@gmail.com>
2025-10-22 16:59:24 +0800

    RevertWithError

--- src/RevertWithError.sol
+++ src/RevertWithError.sol
@@ -7,6 +7,16 @@ contract RevertWithError {
             // revert the function with an error of type `Error(string)`
             // use "RevertRevert" as error message
             // Hint: The error type is a predefined four bytes. See https://www.rareskills.io/post/try-catch-solidity
+
+            // Error(string) selector: 0x08c379a0
+            mstore(0x00, 0x08c379a000000000000000000000000000000000000000000000000000000000)
+            // offset
+            mstore(0x04, 0x20)
+            // length of "RevertRevert"
+            mstore(0x24, 0xC)
+            // actual data "RevertRevert"
+            mstore(0x44, 0x5265766572745265766572740000000000000000000000000000000000000000)
+            revert(0x00, 0x64)
         }
     }
 }
